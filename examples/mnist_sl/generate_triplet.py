a24aa5f6883b83bc9fd55014dcd7746cc102dd02
WAN,Ji <wanji@live.com>
2016-01-10 17:31:12 +0800

    more efficient triplets generation code

--- examples/mnist_sl/generate_triplet.py
+++ examples/mnist_sl/generate_triplet.py
@@ -13,6 +13,7 @@ DESCRIPTION = """
 import os
 import argparse
 import logging
+import random
 
 import numpy as np
 
@@ -33,9 +34,9 @@ def getargs():
         description=DESCRIPTION,
         formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('ori_lst', type=str,
-                        help='IP address of server')
+                        help='original image list')
     parser.add_argument('tri_lst', type=str,
-                        help='IP address of server')
+                        help='triplet list')
     parser.add_argument('num_tri', type=int, nargs='?', default=1000000,
                         help='num of triplets')
     parser.add_argument("--log", type=str, default="INFO",
@@ -48,14 +49,21 @@ def main(args):
     """ Main entry.
     """
     logging.info("Loading image lists ...")
+    v_info = []
     img_lst = dict()
     with open(args.ori_lst, 'r') as orif:
-        for line in orif:
-            impath, imid = line.split()
-            if imid not in img_lst:
-                img_lst[imid] = []
-            img_lst[imid].append(impath)
+        v_info = [line.split() for line in orif]
+    for impath, label in v_info:
+        if label not in img_lst:
+            img_lst[label] = []
+        img_lst[label].append(impath)
     logging.info("Done!")
+
+    num_img = len(v_info)
+    v_imgid = range(num_img)
+    v_negid = range(num_img)
+    qry_idx = num_img
+    neg_idx = -1
 
     logging.info("Generating triplets ...")
     with open(args.tri_lst, 'w') as trif:
@@ -63,10 +71,31 @@ def main(args):
             if i % 10000 == 0:
                 logging.info("\tfinished %6.2f%% of %d" % (
                     100.0 * i / args.num_tri, args.num_tri))
-            c1, c2 = np.random.choice(img_lst.keys(), 2)
-            qry, pos = np.random.choice(img_lst[c1], 2)
-            neg = np.random.choice(img_lst[c2])
+
+            if qry_idx == num_img:
+                random.shuffle(v_imgid)
+                qry_idx = 0
+
+            qry_imgid = v_imgid[qry_idx]
+            qry, label = v_info[qry_imgid]
+            for j in xrange(10):
+                pos = random.choice(img_lst[label])
+                if pos != label:
+                    break
+
+            while True:
+                neg_idx += 1
+                if neg_idx == num_img:
+                    random.shuffle(v_negid)
+                    neg_idx = 0
+                neg_imgid = v_negid[neg_idx]
+                neg, neg_label = v_info[neg_imgid]
+                if neg_label != label:
+                    break
+
             trif.write("%s\t%s\t%s\n" % (qry, pos, neg))
+
+            qry_idx += 1
     logging.info("\tfinished 100.00%% of %d" % (args.num_tri))
     logging.info("Done!")
 
