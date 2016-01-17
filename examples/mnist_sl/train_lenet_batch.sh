01ba840c1e9d9389233fd2cf034c9d51333a3707
WAN,Ji <wanji@live.com>
2016-01-17 21:49:47 +0800

    change back to smaller batch size

--- examples/mnist_sl/train_lenet_batch.sh
+++ examples/mnist_sl/train_lenet_batch.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+DATE=`date "+%y.%m.%d_%H.%M.%S"`
+
 ./build/tools/caffe train \
   --solver=examples/mnist_sl/lenet_batch_solver.prototxt \
-  -gpu 0
+  -gpu 0 \
+  2>&1 | tee examples/mnist_sl/lenet_batch_solver.$DATE.log
--- examples/mnist_sl/train_lenet_naive.sh
+++ examples/mnist_sl/train_lenet_naive.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 
-./build/tools/caffe train --solver=examples/mnist_sl/lenet_naive_solver.prototxt -gpu 1
+DATE=`date "+%y.%m.%d_%H.%M.%S"`
+
+./build/tools/caffe train \
+  --solver=examples/mnist_sl/lenet_naive_solver.prototxt \
+  -gpu 1 \
+  2>&1 | tee examples/mnist_sl/lenet_naive_solver.$DATE.log
